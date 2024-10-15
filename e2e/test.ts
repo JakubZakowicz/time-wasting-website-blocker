7baf28bdc1df3c34e89d70e8154c18bb12a33626
Jakub Zakowicz <j.zakowicz99@gmail.com>
2024-10-15 11:11:16 +0200

    change package.json scripts

--- e2e/test.ts
+++ e2e/test.ts
@@ -7,7 +7,6 @@ const pathToExtension = path.join(__dirname, '..', 'dist');
   const browser = await puppeteer.launch({
     headless: false,
     devtools: true,
-    defaultViewport: null,
     args: [
       `--disable-extensions-except=${pathToExtension}`,
       `--load-extension=${pathToExtension}`,
@@ -16,12 +15,4 @@ const pathToExtension = path.join(__dirname, '..', 'dist');
 
   const page = await browser.newPage();
   await page.goto('https://www.gamesradar.com/far-cry-6-best-gear-builds/');
-
-  const workerTarget = await browser.waitForTarget(
-    target =>
-      target.type() === 'service_worker' &&
-      target.url().endsWith('background.js')
-  );
-
-  await workerTarget.worker();
 })();
