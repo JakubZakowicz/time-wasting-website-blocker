108a365f90bd1d1e2092f6a50faa39b521790d0a
Jakub Zakowicz <j.zakowicz99@gmail.com>
2024-10-18 19:52:57 +0200

    #2 - add on/off badge to icon

--- src/popup.ts
+++ src/popup.ts
@@ -1,3 +1,10 @@
+const updateIcon = (isEnabled: boolean) => {
+  chrome.action.setBadgeText({ text: isEnabled ? 'on' : 'off' });
+  chrome.action.setBadgeBackgroundColor({
+    color: isEnabled ? '#00FF00' : '#FF0000',
+  });
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
   const toggleSwitch = document.getElementById(
     'toggleSwitch'
@@ -5,11 +12,13 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   chrome.storage.local.get('isEnabled', data => {
     toggleSwitch.checked = data.isEnabled || false;
+    updateIcon(data.isEnabled);
   });
 
   toggleSwitch.addEventListener('change', async event => {
     const target = event.target as HTMLInputElement;
     const isEnabled = target.checked;
+    updateIcon(isEnabled);
     chrome.storage.local.set({ isEnabled });
   });
 });
