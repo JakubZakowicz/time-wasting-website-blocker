56adcbb07241d831c3faa6a5f7532223d2816052
Jakub Zakowicz <j.zakowicz99@gmail.com>
2024-10-19 17:14:22 +0200

    #2 - add toggling status container view

--- src/popup.ts
+++ src/popup.ts
@@ -1,8 +1,17 @@
-const updateIcon = (isEnabled: boolean) => {
+const updateIcon = (isEnabled: boolean): void => {
   chrome.action.setBadgeText({ text: isEnabled ? 'on' : 'off' });
   chrome.action.setBadgeBackgroundColor({
     color: isEnabled ? '#00FF00' : '#FF0000',
   });
+};
+
+const updateStatusContainer = (isEnabled: boolean): void => {
+  const statusContainer = document.querySelector('.status-container');
+  if (isEnabled) {
+    statusContainer?.classList.add('show');
+  } else {
+    statusContainer?.classList.remove('show');
+  }
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
@@ -13,12 +22,14 @@ document.addEventListener('DOMContentLoaded', async () => {
   chrome.storage.local.get('isEnabled', data => {
     toggleSwitch.checked = data.isEnabled || false;
     updateIcon(data.isEnabled);
+    updateStatusContainer(data.isEnabled);
   });
 
   toggleSwitch.addEventListener('change', async event => {
     const target = event.target as HTMLInputElement;
     const isEnabled = target.checked;
     updateIcon(isEnabled);
+    updateStatusContainer(isEnabled);
     chrome.storage.local.set({ isEnabled });
   });
 });
